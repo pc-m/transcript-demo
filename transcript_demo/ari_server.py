8320ac1e08e31fb8fccdd93003ab6c59b566ea9c
Pascal Cadotte Michaud <pcm@wazo.io>
2019-10-25 09:34:15 -0400

    refactor: remove asyncio to be able to share as much as possible

--- transcript_demo/ari_server.py
+++ transcript_demo/ari_server.py
@@ -1,24 +1,26 @@
 # Copyright 2019 The Wazo Authors  (see the AUTHORS file)
 # SPDX-License-Identifier: GPL-3.0-or-later
 
-import asyncio
-import aiohttp
+import socket
 import logging
-import uuid
 import sys
+import requests
+
+from queue import Queue
+from threading import Thread
+
 from google.cloud import speech
 from google.cloud.speech import enums
 from google.cloud.speech import types
-from aiohttp_requests import requests
+
 from . import ARI_URL, ARI_USERNAME, ARI_PASSWORD, APPLICATION
 from .output import Output
 
 logging.basicConfig(level=logging.DEBUG)
 
-SERVER_HOST = '127.0.0.1'
-SERVER_PORT = 12222
-EXTERNAL_MEDIA_URL = '/'.join([ARI_URL, 'ari', 'channels', 'externalMedia'])
-CHANNEL_ID = str(uuid.uuid4())
+LISTEN_ADDRESS = '127.0.0.1'
+LISTEN_PORT = 12222
+
 GOOGLE_SPEECH_CREDS_FILENAME = '/root/google_speech_creds.json'
 SPEECH_CLIENT = speech.SpeechClient.from_service_account_file(
     filename=GOOGLE_SPEECH_CREDS_FILENAME,
@@ -32,23 +34,42 @@ STREAMING_CONFIG = types.StreamingRecognitionConfig(
 )
 
 output = None
+DONE = object()
 
 
-class ExternalMediaServer:
-    def __init__(self, queue):
-        self._queue = queue
-
-    def connection_made(self, transport):
-        self.transport = transport
-
-    def datagram_received(self, data, addr):
-        self._queue.put_nowait(data[12:])
+def serve(queue):
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.bind((LISTEN_ADDRESS, LISTEN_PORT))
+    while True:
+        data, _ = sock.recvfrom(4096)
+        payload = data[12:]
+        queue.put_nowait(payload)
 
 
-async def fetch_transcription(buf):
-    request = types.StreamingRecognizeRequest(audio_content=buf)
+def transcribe(queue):
+    step = 64 * 1024
+    transcribe_threshold = step
+    buffer = b''
+    while True:
+        data = queue.get()
+        try:
+            if data == DONE:
+                return
+            buffer += data
+            written = len(buffer)
+            if written >= transcribe_threshold:
+                transcribed = do_transcription(buffer)
+                transcribe_threshold = written + step
+                output.write(transcribed)
+        finally:
+            queue.task_done()
+
+
+def do_transcription(data):
+    logging.debug('Sending %s to the speech API', len(data))
+    request = types.StreamingRecognizeRequest(audio_content=data)
     responses = list(SPEECH_CLIENT.streaming_recognize(STREAMING_CONFIG, [request]))
-    logging.debug('%s', responses)
+    logging.debug('responses: %s', responses)
 
     output = '\n'
     for response in responses:
@@ -59,67 +80,38 @@ async def fetch_transcription(buf):
                 continue
             output += '{}\n'.format(result.alternatives[0].transcript)
 
-    logging.debug('%s', output)
+    logging.debug('final output: %s', output)
     return output
-
-
-async def transcribe(queue):
-    buffer = b''
-    step = 64 * 1024
-    threshold = step
-    while True:
-        data = await queue.get()
-        buffer += data
-        written = len(buffer)
-        if written >= threshold:
-            logging.debug('transcribing...')
-            transcribed = await fetch_transcription(buffer)
-            threshold = written + step
-            output.write(transcribed)
-
-
-async def create_external_media():
-    await asyncio.sleep(1)
-
-    logging.debug('POSTing %s', EXTERNAL_MEDIA_URL)
-    response = await requests.post(
-        EXTERNAL_MEDIA_URL,
-        auth=aiohttp.BasicAuth(ARI_USERNAME, ARI_PASSWORD),
-        params={
-            'channelId': CHANNEL_ID,
-            'app': APPLICATION,
-            'external_host': '{}%3A{}'.format(SERVER_HOST, SERVER_PORT),
-            'format': 'ulaw',
-        },
-    )
-    result = await response.text()
-    logging.debug('result: %s', result)
-
-
-async def start(host, port):
-    loop = asyncio.get_running_loop()
-    queue = asyncio.Queue()
-
-    transport, protocol = await loop.create_datagram_endpoint(
-        lambda: ExternalMediaServer(queue),
-        local_addr=(host, port),
-    )
-
-    logging.debug('launching task')
-    tasks = [
-        asyncio.create_task(create_external_media()),
-        asyncio.create_task(transcribe(queue)),
-    ]
-
-    asyncio.gather(*tasks)
-
-    await asyncio.sleep(3600)
 
 
 def main():
     global output
 
     logging.debug('Starting %s', sys.argv[0])
+    queue = Queue()
     output = Output()
-    asyncio.run(start(SERVER_HOST, SERVER_PORT))
-    logging.debug('bye')
+
+    transcriber_thread = Thread(target=transcribe, args=(queue,))
+    transcriber_thread.start()
+
+    url = '/'.join([ARI_URL, 'ari', 'channels', 'externalMedia'])
+    response = requests.post(
+        url,
+        auth=(ARI_USERNAME, ARI_PASSWORD),
+        data={
+            'app': APPLICATION,
+            'external_host': '{}:{}'.format('127.0.0.1', LISTEN_PORT),
+            'format': 'ulaw',
+        }
+    )
+
+    try:
+        serve(queue)
+    except KeyboardInterrupt:
+        pass
+    finally:
+        url = '/'.join([ARI_URL, 'ari', 'channels', response.json()['channel']['id']])
+        requests.delete(url, auth=(ARI_USERNAME, ARI_PASSWORD))
+
+        queue.put_nowait(DONE)
+        transcriber_thread.join()
